3e5f37469bad67f3b4fbc26c56c536416ec40c2e
wendy705 <wendy705@users.noreply.github.com>
2020-03-15 21:58:36 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ cash.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+//get user input for change owed
+    float change_owed;
+    do
+    {
+    change_owed = get_float("What's the change you are owed?\n$");
+    }
+    while (change_owed < 0 );
+    int cents_owed = round (change_owed * 100);
+   
+
+//get remainder for every coin
+    int quarters = cents_owed / 25;
+    int dimes = (cents_owed % 25) / 10;
+    int nickels = ((cents_owed % 25) % 10) / 5;
+    int pennies = ((cents_owed % 25) % 10) % 5;
+    
+    printf("%i\n", quarters + dimes + nickels + pennies);
+}
