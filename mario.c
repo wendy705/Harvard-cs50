eb16371d34ca0663775a78b84c4b28584b6b0776
wendy705 <wendy705@users.noreply.github.com>
2020-03-15 20:56:22 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -3,30 +3,37 @@
 
 int pyramid(int n);
 
-int main(void){
+//getting user input for pyramid height
+int main(void)
+{
     int height;
 
-    do {
+    do 
+    {
         height = get_int("Please enter a number between one and eight: ");
-    } while(height < 1 || height > 8);
+    } 
+    //setting limits for pyramid height
+    while (height < 1 || height > 8);
     
     return pyramid(height);
 }
 
 int pyramid(int n)
-   {
+{
 //Print hashes iteration
     for (int i = 1; i <= n; i++)
+    {
+        for (int j = (n - i); j >= 1; j--)
         {
-            for (int j = (n - i); j >= 1; j--)
-                {
-                    printf(" ");
-                }
-            for (int k = 1; k < (i + 1); k++)
-                {
-                    printf("#"); 
-                }
-            printf("\n");
+            printf(" ");
         }
+
+        for (int k = 1; k < (i + 1); k++)
+        {    
+            printf("#"); 
+        }
+        printf("\n");
+    }
+    //in case of error, you could throw error
     return 0;
 }
