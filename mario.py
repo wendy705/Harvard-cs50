8ed62fe4fea9ee347b3da761806390eda128c61c
wendy705 <wendy705@users.noreply.github.com>
2020-10-11 17:28:09 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,11 +1,11 @@
 from cs50 import get_int
 
-#ask for user input until it receives a valid one
+# ask for user input until you get a valid one
 height = 0
 while (height > 8 or height < 1):
     height = get_int("Height:\n")
 
-#print a pyramid of as many hashes as indicated by the user
+# print a pyramid of as many hashes as indicated by the user
 for i in range(1, height + 1):
     print(" " * (height - i), end="")
     print("#" * i, end="  ")
